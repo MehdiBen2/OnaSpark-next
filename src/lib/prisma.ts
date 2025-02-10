e36849069ac6e844c8c449a57177936dac4d316d
MehdiBen2 <medyben12@gmail.com>
2025-02-10 10:24:45 +0100

    edit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
-// Prevent multiple Prisma Client instances in development
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
+const prismaClientSingleton = () => {
+  return new PrismaClient({
     log: process.env.NODE_ENV === 'development' 
       ? ['query', 'error', 'warn'] 
       : ['error']
   })
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+export const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
 export default prisma
